eecf526998eae439972401ec1070a3ef651d85fd
Viktor <ricchiuto.viktor@gmail.com>
2021-05-13 08:10:46 +0200

    remove server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import './style.css';
-// import GameServer from './server/GameServer';
 import Sketch from './client/Sketch';
 import P5 from 'p5';
-
-// let server = new GameServer();
-// server.start();
 
 new P5(Sketch);
