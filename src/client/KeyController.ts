410d05c7c5bc9cc9cee65b4b5cfa2ea763c67e3c
Viktor <ricchiuto.viktor@gmail.com>
2021-05-15 06:52:09 +0200

    add alternative keyboard controls

--- src/client/KeyController.ts
+++ src/client/KeyController.ts
@@ -1,28 +1,42 @@
 import { forIn } from 'lodash';
 import p5 from 'p5';
-import Ship from '../core/Ship';
+import { GameState } from '../core/GameEngine';
 
 type Func = () => void;
 
 enum Keys {
   ARROW_LEFT = 37,
   ARROW_RIGHT = 39,
-  ARROW_UP = 38,
-  ARROW_DOWN = 40,
+  LETTER_J = 74,
+  LETTER_L = 76,
+  NUMPAD_4 = 100,
+  NUMPAD_5 = 101,
+  NUMPAD_6 = 102,
+  LETTER_Q = 81,
+  LETTER_E = 69,
+  // ARROW_UP = 38,
+  // ARROW_DOWN = 40,
   SPACE_BAR = 32
 }
 
 class KeyController {
-  private ship: Ship;
+  private state: GameState;
   private commandsMap: Record<Keys, Func>;
-  constructor(ship: Ship) {
-    this.ship = ship;
+  constructor(state: GameState) {
+    this.state = state;
     this.commandsMap = {
-      [Keys.ARROW_LEFT]: ship.turnLeft,
-      [Keys.ARROW_RIGHT]: ship.turnRight,
-      [Keys.ARROW_UP]: ship.accelerate,
-      [Keys.ARROW_DOWN]: ship.decelerate,
-      [Keys.SPACE_BAR]: ship.fire
+      [Keys.ARROW_LEFT]: state.ship.turnLeft,
+      [Keys.LETTER_J]: state.ship.turnLeft,
+      [Keys.NUMPAD_4]: state.ship.turnLeft,
+      [Keys.LETTER_Q]: state.ship.turnLeft,
+      [Keys.ARROW_RIGHT]: state.ship.turnRight,
+      [Keys.LETTER_L]: state.ship.turnRight,
+      [Keys.NUMPAD_6]: state.ship.turnRight,
+      [Keys.LETTER_E]: state.ship.turnRight,
+      // [Keys.ARROW_UP]: state.ship.accelerate,
+      // [Keys.ARROW_DOWN]: state.ship.decelerate,
+      [Keys.SPACE_BAR]: state.ship.decelerate,
+      [Keys.NUMPAD_5]: state.ship.decelerate
     };
   }
 
@@ -30,7 +44,7 @@ class KeyController {
     forIn(this.commandsMap, (command, key) => {
       const keyCode = parseInt(key) as Keys;
       if (p5.keyIsDown(keyCode)) {
-        this.commandsMap[keyCode].call(this.ship);
+        this.commandsMap[keyCode].call(this.state.ship);
       }
     });
   }
