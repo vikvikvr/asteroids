e23a4e0d9736f4859df52d366ff73c414f1a35e9
Viktor <ricchiuto.viktor@gmail.com>
2021-05-16 19:04:48 +0200

    remove life info from gui

--- src/client/GUI.ts
+++ src/client/GUI.ts
@@ -2,7 +2,6 @@ import P5 from 'p5';
 import { SpawnerEtas } from '../core/Spawner';
 import { ColorsMap } from './colors';
 import GameEngine, { GameState } from '../core/GameEngine';
-import Ship from '../core/Ship';
 
 const SPACING = 20;
 
@@ -18,20 +17,8 @@ class GUI {
 
   public draw(engine: GameEngine) {
     this.p5.textSize(20);
-    this.drawShipInfo(engine.state.ship);
     // this.drawTimersInfo(etas);
     this.drawScore(engine.state);
-  }
-
-  private drawShipInfo(ship: Ship) {
-    let { life } = ship;
-    let { p5 } = this;
-    let [x, y] = [SPACING, SPACING];
-    p5.fill('white');
-    p5.noStroke();
-    p5.textAlign(p5.LEFT);
-    p5.text(`life: ${percentToString(life)}`, x, y);
-    // p5.text(`speed: ${percentToString(speed)}`, x, y + SPACING);
   }
 
   private drawTimersInfo(etas: SpawnerEtas) {
@@ -46,10 +33,6 @@ class GUI {
     p5.text(state.score, p5.windowWidth / 2, SPACING);
     p5.text(`Level ${state.level}`, p5.windowWidth / 2, SPACING * 2);
   }
-}
-
-function percentToString(value: number): string {
-  return (value * 100).toFixed(0) + '%';
 }
 
 export default GUI;
