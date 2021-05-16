2ff427101e1f12dc0d6f53edc9a13fee8d6f1b34
Viktor <ricchiuto.viktor@gmail.com>
2021-05-16 12:25:10 +0200

    add drawExplostionShard to animations

--- src/client/animations.ts
+++ src/client/animations.ts
@@ -1,9 +1,15 @@
 import P5 from 'p5';
-import { TextAnimation } from './Animation';
+import GameObject from '../core/GameObject';
+import { ExplosionAnimation, TextAnimation } from './Animation';
 
 export function drawTextAnimation(p5: P5, animation: TextAnimation): void {
   const { currentFrame, frameCount, text } = animation;
   const alpha = (1 - currentFrame / frameCount) * 255;
   p5.fill(255, 255, 255, alpha);
   p5.text(text, 0, 0);
+}
+
+export function drawExplostionShard(p5: P5) {
+  p5.fill('white');
+  p5.circle(0, 0, 20);
 }
