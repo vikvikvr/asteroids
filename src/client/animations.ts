8d218fa0ed2d501c49df248b020d84cad0101231
Viktor <ricchiuto.viktor@gmail.com>
2021-05-18 07:07:04 +0200

    update explosion shard shape drawer

--- src/client/animations.ts
+++ src/client/animations.ts
@@ -1,14 +1,6 @@
 import P5 from 'p5';
-import { AsteroidSize } from '../core/Asteroid';
-import { GameTemperature } from '../core/GameEngine';
-import GameObject from '../core/GameObject';
+import Shard from '../core/Shard';
 import { TextAnimation } from './Animation';
-
-const colorMap: Record<GameTemperature, string[]> = {
-  normal: ['#009688', '#00897b', '#00796b', '#00695c'],
-  high: ['#f44336', '#e53935', '#d32f2f', '#c62828'],
-  low: ['#2196f3', '#1e88e5', '#1976d2', '#1565c0']
-};
 
 export function drawTextAnimation(p5: P5, animation: TextAnimation): void {
   const { currentFrame, frameCount, text } = animation;
@@ -17,12 +9,7 @@ export function drawTextAnimation(p5: P5, animation: TextAnimation): void {
   p5.text(text, 0, 0);
 }
 
-export function drawExplostionShard(
-  p5: P5,
-  shard: GameObject,
-  size: AsteroidSize,
-  temperature: GameTemperature
-) {
-  p5.fill(colorMap[temperature][0]);
+export function drawExplostionShard(p5: P5, shard: Shard) {
+  p5.fill(shard.color);
   p5.circle(0, 0, shard.hitBoxRadius);
 }
